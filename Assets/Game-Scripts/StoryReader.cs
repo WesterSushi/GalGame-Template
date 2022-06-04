a60587a608a28de98431920cac73f850f510a9fd
CreamShanNei <shannei@elyart.cn>
2022-06-05 01:43:32 +0800

    ♻️<chore>(Create basic file)-Add-some-basic-file
    like story file...
    
    edit-by:BakaShanNei

---
+++ Assets/Game-Scripts/StoryReader.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryReader : MonoBehaviour
+{
+    public TextAsset zh_CN, zh_TW, en_US, jp_JP;
+    private void Start()
+    {
+        
+    }
+}
